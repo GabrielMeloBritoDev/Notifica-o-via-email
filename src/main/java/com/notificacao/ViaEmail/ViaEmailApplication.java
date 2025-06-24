ebea46e12e3d82f1eeb03303ee1d13d9637524f3
Gabriel Melo <gabrielmelobrito63@gmail.com>
2025-06-24 20:00:20 -0300

    commit inicial

---
+++ src/main/java/com/notificacao/ViaEmail/ViaEmailApplication.java
@@ -0,0 +1,13 @@
+package com.notificacao.ViaEmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViaEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notificacao/ViaEmail/ViaEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notificacao.ViaEmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViaEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
